137861fcc0a5d7883dbb94797d7332a2c740e82a
Yokub <yoqub.madixanov@outlook.com>
2026-05-14 18:58:20 +0500

    frontend: adding text input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import TextInput from "./components/TextInput";
 import WelcomingSection from "./components/WelcomingSection";
 
 export default function App() {
   return (
-    <main className="w-full md:max-w-5xl lg:max-w-300 xl:max-w-337.5 min-h-screen flex items-center p-5">
+    <main className="w-full md:max-w-5xl lg:max-w-300 xl:max-w-337.5 min-h-screen flex flex-col justify-center p-5">
       <WelcomingSection />
+      <TextInput />
     </main>
   );
 }
---
+++ frontend/src/components/TextInput.tsx
@@ -0,0 +1,28 @@
+import { Mic, ArrowRight } from "lucide-react";
+
+export default function TextInput() {
+    return (
+        <div className="fixed bottom-6 left-1/2 -translate-x-1/2 w-full max-w-2xl px-4">
+            <div className="flex items-center gap-3 rounded-2xl border border-white/10 bg-surface-light px-4 py-3 shadow-lg backdrop-blur-md">
+                
+                {/* mic */}
+                <button className="text-white/70 hover:text-white transition">
+                <Mic size={22} />
+                </button>
+
+                {/* input */}
+                <input
+                type="text"
+                placeholder="Send a message..."
+                className="flex-1 bg-transparent outline-none text-white placeholder:text-white/50"
+                />
+
+                {/* send */}
+                <button className="text-white/70 hover:text-white transition">
+                <ArrowRight size={22} />
+                </button>
+
+            </div>
+            </div>
+    )
+}
