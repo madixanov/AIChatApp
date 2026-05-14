2ebadc5a9eaf63e19d955eae6bd29fa382fd0130
Yokub <yoqub.madixanov@outlook.com>
2026-05-14 18:19:41 +0500

    creating frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind Works
+      </h1>
+    </div>
+  );
+}
